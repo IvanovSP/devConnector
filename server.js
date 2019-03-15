e823d38c2bcd0cc8008b21d98e37b567a3a002b0
ivanovSPvirtru <sivanov.ctr@virtru.com>
2019-03-15 13:00:40 +0200

    routs added

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const espress = require('express');
-
+const exspress = require('express');
 const mysql = require('./mysql-promise');
-
-const app = espress();
-
 const db = require('./config/keys');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
+const app = exspress();
 mysql.connect(db);
 
 app.get('/', (res) => {
   res.send('hello');
 });
 
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
