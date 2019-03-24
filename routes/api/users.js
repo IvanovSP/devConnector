94af1120172c570c7b2a6d0374fc6016f275efc7
ivanovSPvirtru <sivanov.ctr@virtru.com>
2019-03-24 21:25:21 +0200

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const bcript = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const mysql = require('../../mysql-promise');
 
 const router = express.Router();
 
+// @route     GET api/users/register
+// @desc      Register User
+// @access    Public
 router.post('/register', async (req, res) => {
   try {
     const {
       email, name, profStatus, currentCompany, city, github, bio, password,
     } = req.body;
-    console.log(email, name, password);
 
     const [emailIsExist] = await mysql.query(
       `SELECT email FROM user WHERE email = "${email}" LIMIT 1`,
@@ -29,8 +31,8 @@ router.post('/register', async (req, res) => {
       );
     }
 
-    const salt = await bcript.genSalt(10);
-    const passwordHashed = await bcript.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const passwordHashed = await bcrypt.hash(password, salt);
 
     await mysql.query(
       'INSERT INTO user (handle, name, prof_status_id, current_company_id, city, github_username, bio, email, password) VALUES ?',
@@ -44,5 +46,32 @@ router.post('/register', async (req, res) => {
     return res.sendStatus(500);
   }
 });
+
+
+// @route     GET api/users/login
+// @desc      Login User / Returning JWT  Token
+// @access    Protected
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [user] = await mysql.query(
+      `SELECT password FROM user WHERE email = "${email}" LIMIT 1`,
+    );
+    if (!user) {
+      return res.sendStatus(404).json({ email: 'User not found' });
+    }
+    const passwordIsMatch = await bcrypt.compare(password, user.password);
+
+    if (passwordIsMatch) {
+      return res.json({ msg: 'Success' });
+    }
+    return res.sendStatus(400).json({ password: 'Password incorrect' });
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(500);
+  }
+});
+
 
 module.exports = router;
