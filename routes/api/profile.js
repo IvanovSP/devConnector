31eb4d7465a968719ebfa1ea2958a3381853540d
ivanovSPvirtru <sivanov.ctr@virtru.com>
2019-04-07 12:50:09 +0300

    profile get

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,27 @@
 const express = require('express');
+const passport = require('passport');
+const mysql = require('../../mysql-promise');
 
 const router = express.Router();
 
-router.get('/test', (req, res) => res.json({ msg: 'profile works' }));
+// @route     GET api/profile
+// @desc      Get current user profile
+// @access    Private
+router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const errors = {};
+  try {
+    const [{ password, ...profile }] = await mysql.query(
+      `SELECT * FROM user WHERE email = "${req.user.id}" LIMIT 1`,
+    );
+    if (!profile) {
+      errors.noprofiles = 'No user found';
+      return res.status(404).json(errors);
+    }
+    return res.json(profile);
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(500);
+  }
+});
 
 module.exports = router;
