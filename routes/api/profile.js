5cd68e6e05ec56ca1dc6de3dc85a22c7c53cf285
ivanovSPvirtru <sivanov.ctr@virtru.com>
2019-04-30 22:01:01 +0300

    skillset post/delete added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -54,4 +54,29 @@ router.delete('/:userId/social/:socialId', passport.authenticate('jwt', { sessio
   }
 });
 
+router.post('/:userId/skills/:skillId', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const { userId, skillId } = req.params;
+  try {
+    const [isExist] = await mysql.query(`SELECT skillset.id FROM skillset WHERE (user_id = '${userId}' AND skill_id = '${skillId}')`);
+    if (isExist) return res.status(409).json({ isExist: 'record already exist' });
+    await mysql.query(`INSERT INTO skillset (user_id, skill_id) VALUES ('${userId}', '${skillId}')`);
+
+    return res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(500);
+  }
+});
+
+router.delete('/:userId/skills/:skillsId', passport.authenticate('jwt', { session: false }), async (req, res) => {
+  const { userId, skillId } = req.params;
+  try {
+    await mysql.query(`DELETE FROM skillset WHERE (user_id = '${userId}' AND skill_id = '${skillId}')`);
+    return res.sendStatus(200);
+  } catch (e) {
+    console.log(e);
+    return res.sendStatus(500);
+  }
+});
+
 module.exports = router;
